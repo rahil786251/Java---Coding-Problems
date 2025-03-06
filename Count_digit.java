de601b82a9ea6a39611b94fac4af35c5584b8157
Shaik Rahil Basha <rahilbasha327@gmail.com>
2025-03-06 15:26:53 +0530

    Create Count_digit.java

---
+++ Count_digit.java
@@ -0,0 +1,14 @@
+//This program counts the number of digits in a given integer (n = 12345) and prints the result.
+
+
+
+public class Count_digit {
+	public static void main(String[] args) {
+		int n=12345;
+		System.out.println("no.of digits = ");
+		String s=n+"";
+		System.out.print(s.length());
+	}
+	
+}
+		
